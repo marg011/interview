67ea00e62cc3e3e4a9dd65f82e0f779489b182f0
margo <margo171190@mail.ru>
2023-12-29 22:52:39 +0400

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringinterview/SkyproSpringInterviewApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringInterviewApplication.class, args);
+	}
+
+}
